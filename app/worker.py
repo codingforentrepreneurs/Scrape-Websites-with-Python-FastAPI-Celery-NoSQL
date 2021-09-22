561dd9a8336a5692d24ff31b6583c504362353a8
CFE <hello@teamcfe.com>
2021-09-22 14:09:14 -0500

    18 - Running Periodic Tasks

--- app/worker.py
+++ app/worker.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 from celery.signals import (
     beat_init,
     worker_process_init,
@@ -40,6 +41,22 @@ beat_init.connect(celery_on_startup)
 worker_process_init.connect(celery_on_startup)
 
 
+# celery --app app.worker.celery_app worker --beat -s celerybeat-schedule --loglevel INFO
+
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, *args, **kwargs):
+    # sender.add_periodic_task(1, random_task.s("abc"), expires=10)
+    # sender.add_periodic_task(
+    #     crontab(hour=8, minute=0, day_of_week=2), 
+    #     random_task.s("abc")
+    #     )
+    sender.add_periodic_task(
+        crontab(minute="*/5"),
+        scrape_products.s()
+    )
+
+
+
 @celery_app.task
 def random_task(name):
     print(f"Who throws a shoe. Honestly {name}.")
@@ -50,3 +67,16 @@ def random_task(name):
 def list_products():
     q = Product.objects().all().values_list("asin", flat=True)
     print(list(q))
+
+
+@celery_app.task
+def scrape_asin(asin):
+    print(asin)
+
+
+@celery_app.task
+def scrape_products():
+    print("Doing scraping")
+    q = Product.objects().all().values_list("asin", flat=True)
+    for asin in q:
+        scrape_asin.delay(asin)
